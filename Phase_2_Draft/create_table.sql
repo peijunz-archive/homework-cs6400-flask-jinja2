6e0953a59a033c2a475314b3d0f671c861465f4e
Tom Huang <tomhuang@Toms-MacBook-Pro.local>
2018-06-24 21:32:11 -0400

    updates

--- Phase_2_Draft/create_table.sql
+++ Phase_2_Draft/create_table.sql
@@ -1,28 +1,28 @@
-CREATE TABLE 'User'(
+CREATE TABLE `User`(
     Username varchar(50) NOT NULL,
     Name varchar(50) NOT NULL,
     Password varchar(50) NOT NULL,
     PRIMARY KEY(Username)
 );
 
-CREATE TABLE 'GovAgencies'(
+CREATE TABLE `GovAgencies`(
     Username varchar(50) NOT NULL,
     AgencyNameLocalOffice varchar(50) NOT NULL,
     PRIMARY KEY(Username),
     FOREIGN KEY(Username)
-        REFERENCES 'User' (Username)
+        REFERENCES `User` (Username)
 );
 
-CREATE TABLE 'Companies'(
+CREATE TABLE `Companies`(
     Username varchar(50) NOT NULL,
     Location varchar(50) NOT NULL,
     NumberOFEmployees int NOT NULL,
     PRIMARY KEY(Username),
     FOREIGN KEY(Username)
-        REFERENCES 'User' (Username)
+        REFERENCES `User` (Username)
 );
 
-CREATE TABLE 'Municipalities'(
+CREATE TABLE `Municipalities`(
     Username varchar(50) NOT NULL,
     City varchar(50) NULL,
     County varchar(50) NULL,
@@ -30,30 +30,30 @@ CREATE TABLE 'Municipalities'(
     Country varchar(50) NULL,
     PRIMARY KEY(Username),
     FOREIGN KEY(Username)
-        REFERENCES 'User' (Username)
+        REFERENCES `User` (Username)
 );
 
-CREATE TABLE 'Individuals'(
+CREATE TABLE `Individuals`(
     Username varchar(50) NOT NULL,
     JobTitle varchar(50) NOT NULL,
     DateHired datetime NOT NULL,
     PRIMARY KEY(Username),
     FOREIGN KEY(Username)
-        REFERENCES 'User' (Username)
+        REFERENCES `User` (Username)
 );
 
-CREATE TABLE 'TimeUnit'(
+CREATE TABLE `TimeUnit`(
     Name varchar(50) NOT NULL,
     PRIMARY KEY(Name)
 );
 
-CREATE TABLE 'ESF'(
+CREATE TABLE `ESF`(
     Number int NOT NULL,
     Description varchar(50) NOT NULL,
     PRIMARY KEY(Number)
 );
 
-CREATE TABLE 'Resources'(
+CREATE TABLE `Resources`(
     ID int NOT NULL,
     Latitude float NOT NULL,
     Longitude float NOT NULL,
@@ -65,38 +65,38 @@ CREATE TABLE 'Resources'(
     Username varchar(50) NOT NULL,
     PRIMARY KEY(Id),
     FOREIGN KEY (PrimaryESFNumber)
-        REFERENCES 'ESF' (Number),
+        REFERENCES `ESF` (Number),
     FOREIGN KEY (Name)
-        REFERENCES 'TimeUnit' (Name),
+        REFERENCES `TimeUnit` (Name),
     FOREIGN KEY (Username)
-        REFERENCES 'User' (Username)
+        REFERENCES `User` (Username)
 );
 
-CREATE TABLE 'AdditionalESF'(
+CREATE TABLE `AdditionalESF`(
     ID int NOT NULL,
     Number int NOT NULL,
     PRIMARY KEY(ID, Number),
     FOREIGN KEY (ID)
-        REFERENCES 'Resources' (ID),
+        REFERENCES `Resources` (ID),
     FOREIGN KEY (Number)
-        REFERENCES 'ESF' (Number)
+        REFERENCES `ESF` (Number)
 );
 
-CREATE TABLE 'Capabilities'(
+CREATE TABLE `Capabilities`(
     ID int NOT NULL,
     CapabilityName varchar(50) NOT NULL,
     PRIMARY KEY(ID, CapabilityName),
     FOREIGN KEY (ID)
-        REFERENCES 'Resources' (ID)
+        REFERENCES `Resources` (ID)
 );
 
-CREATE TABLE 'Declaration'(
+CREATE TABLE `Declaration`(
     Abbreviation char(2) NOT NULL,
     Name varchar(50) NOT NULL,
     PRIMARY KEY(Abbreviation)
 );
 
-CREATE TABLE 'Incident'(
+CREATE TABLE `Incident`(
     Abbreviation char(2) NOT NULL,
     Number int NOT NULL,
     Date datetime NOT NULL,
@@ -106,44 +106,38 @@ CREATE TABLE 'Incident'(
     Username varchar(50) NOT NULL,
     PRIMARY KEY(Abbreviation, Number),
     FOREIGN KEY (Abbreviation)
-        REFERENCES 'Declaration' (Abbreviation),
+        REFERENCES `Declaration` (Abbreviation),
     FOREIGN KEY (Username)
-        REFERENCES 'User' (Username)
+        REFERENCES `User` (Username)
 );
 
-CREATE TABLE 'Requests'(
+CREATE TABLE `Requests`(
     ID int NOT NULL,
     Abbreviation char(2) NOT NULL,
     Number int NOT NULL,
     RequestDate datetime NOT NULL,
     ReturnDate datetime NOT NULL,
     PRIMARY KEY(ID, Abbreviation, Number),
-    FOREIGN KEY (Abbreviation)
-        REFERENCES 'Incident' (Abbreviation),
-    FOREIGN KEY (Number)
-        REFERENCES 'Incident' (Number)
+    FOREIGN KEY (Abbreviation, Number)
+        REFERENCES `Incident` (Abbreviation, Number)
 );
 
-CREATE TABLE 'InUse'(
+CREATE TABLE `InUse`(
     ID int NOT NULL,
     Abbreviation char(2) NOT NULL,
     Number int NOT NULL,
     StartDate datetime NOT NULL,
     ReturnDate datetime NOT NULL,
     PRIMARY KEY(ID),
-    FOREIGN KEY (Abbreviation)
-        REFERENCES 'Incident' (Abbreviation),
-    FOREIGN KEY (Number)
-        REFERENCES 'Incident' (Number)
+    FOREIGN KEY (Abbreviation, Number)
+        REFERENCES `Incident` (Abbreviation, Number)
 );
 
-CREATE TABLE 'LastUsed'(
+CREATE TABLE `LastUsed`(
     ID int NOT NULL,
     Abbreviation char(2) NOT NULL,
     Number int NOT NULL,
     PRIMARY KEY(ID),
-    FOREIGN KEY (Abbreviation)
-        REFERENCES 'Incident' (Abbreviation),
-    FOREIGN KEY (Number)
-        REFERENCES 'Incident' (Number)
+    FOREIGN KEY (Abbreviation, Number)
+        REFERENCES `Incident` (Abbreviation, Number)
 );
