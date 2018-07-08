025da3db1fb113077d72b0f7c99e559e5504c513
unknown <xichengh12@gmail.com>
2018-07-08 17:32:07 -0400

    backend initial files

---
+++ Phase 3 Code/app.py
@@ -0,0 +1,35 @@
+from flask import Flask,request,jsonify,json
+import pymysql
+
+app = Flask(__name__)
+db = pymysql.connect("localhost","user","Mysql123!","cs6400_summer18_team010" )
+ 
+@app.route("/")
+def index():
+        return "Welcome to Emergency Resource Management System Web Service!"
+
+@app.route("/Authenticate")
+def Authenticate():
+        username = request.args.get('UserName')
+        password = request.args.get('Password')
+        cursor = db.cursor()
+        sql = "SELECT * from user where Username='" + username + "' and Password='" + password + "'"
+        result={}
+        try:
+            # Execute the SQL command
+            cursor.execute(sql)
+            # Fetch all the rows in a list of lists.
+            data = cursor.fetchone()
+            if data is None:
+                result['status']='failed'
+            else:
+                result['status']='success'
+            return json.dumps(result)
+        except:
+            print ("Error: unable to fetch data")
+
+        # disconnect from server
+        # db.close()
+
+if __name__ == "__main__":
+        app.run()
