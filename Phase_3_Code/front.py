c0506d867fd25a42c24bd070d7adaf3f1c5035b0
Peijun Zhu <zpj.ustc@gmail.com>
2018-07-18 22:55:00 -0400

    Adding Post

--- Phase_3_Code/front.py
+++ Phase_3_Code/front.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def base_template():
     return redirect("/login.html", code=302)
 
-@app.route("/login.html")
+@app.route("/login.html", methods=['GET', 'POST'])
 def login_page():
-    return render_template("login2.html")
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        return "You input username={}, password={}".format(username, password)
+    else:
+        return render_template("login2.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
