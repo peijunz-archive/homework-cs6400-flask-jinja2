aca9ab20e6e6130181594b6c46dd23ca4969f7bd
Peijun Zhu <zpj.ustc@gmail.com>
2018-07-19 03:30:32 -0400

    Adding navbar

--- Phase_3_Code/front.py
+++ Phase_3_Code/front.py
@@ -1,8 +1,8 @@
-from flask import Flask,request,jsonify,json,render_template,redirect
+from flask import Flask,request,jsonify,json,render_template,redirect,session
 import requests
 
 app = Flask(__name__)
-
+app.secret_key = '_5#y2L"F4Q8dATabASe64OOtEaMOi0]/'
 server = "http://127.0.0.1:5000"
 
 def extract(form, *keys):
@@ -23,18 +23,26 @@ def url_tail(F):
 
 @app.route("/")
 def base_template():
-    return redirect("/login.html", code=302)
+    if 'username' in session:
+        return redirect("/menu.html", code=302)
+    else:
+        return redirect("/login.html", code=302)
 
 @app.route("/login.html", methods=['GET', 'POST'])
 def login_page():
     if request.method == "POST":
         F = extract(request.form)
+        # TODO Check basic validity of username and password e.g. 0 < len < 50
         url = server + '/login?' + url_tail(F)
         r = requests.get(url)
         t = json.loads(r.content)
         return t['status']
     else:
         return render_template("login2.html")
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    return "Test"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5555)
